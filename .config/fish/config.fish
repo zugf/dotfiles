00f91827349b4f5d550aaea70d63b6ce2744c9a7
zugf <florian.zug@zeppelin.com>
2022-07-08 17:56:43 +0200

    add ssh-agent

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,12 +3,6 @@ set -g fish_user_paths "$CHECKOUT/bin" $fish_user_paths
 set -x DOCKER_DEFAULT_PLATFORM linux/amd64
 oh-my-posh init fish --config ~/.config/poshtheme.omp.json | source
 
-if status is-interactive
+if status is-interactive   
     envsource ~/.splunkbuild.env
-
-    if test -z (pgrep ssh-agent | string collect)
-        eval (ssh-agent -c)
-        set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
-        set -Ux SSH_AGENT_PID $SSH_AGENT_PID
-    end
 end
